14a51c0bbf4e2c7af1a3d5a62217493fc2c28ae1
harise100 <harald.sebler@gmx.net>
2018-05-27 22:18:02 +0200

    Add dotOS

---
+++ dot.mk
@@ -0,0 +1,9 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/starlte/full_starlte.mk)
+
+# Inherit some common dotOS stuff.
+#$(call inherit-product, vendor/dot/config/common.mk)
+
+PRODUCT_NAME := dot_starlte
