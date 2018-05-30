f379409ba6c62840f88e4e392bf1ea5cca7495c2
Harald S <harald.sebler@gmx.net>
2018-05-30 20:52:30 +0200

    now in its own branch

--- dot.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2018 The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-
-$(call inherit-product, device/samsung/starlte/full_starlte.mk)
-
-# Inherit some common dotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
-
-PRODUCT_NAME := dot_starlte
